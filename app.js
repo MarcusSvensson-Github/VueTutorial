cadb18e9983e70bdd11d72b7e8e27ec2c8dc52dc
MarcusSvensson-Github <82384993+MarcusSvensson-Github@users.noreply.github.com>
2023-10-24 10:25:23 +0200

    v-for

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const app = Vue.createApp({
                 show: false,
                 eventTracker: 0,
                 x: 0,
-                y: 0
+                y: 0,
+                showMouseover: false,
+                books: [
+                    {author: "Dan brown", title: "da vinci koden"},
+                    {author: "ban brown", title: "c# koden"},
+                    {author: "stan brown", title: "koden"}
+                    ]
         }
 
     },
