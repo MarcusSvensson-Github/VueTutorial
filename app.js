b663072565daf698a6aa3cdc109eb018362cf831
MarcusSvensson-Github <82384993+MarcusSvensson-Github@users.noreply.github.com>
2023-10-24 10:06:46 +0200

    mousover v-on

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const app = Vue.createApp({
         return {title: "Welcome to the vue tutorial", 
                 age: 5, 
                 newTitle: "",
-                show: false
+                show: false,
+                eventTracker: 0,
+                x: 0,
+                y: 0
         }
 
     },
     methods: {
         changeTitle(newTitle){
             this.title = newTitle
-        }        
+        },
+        positionHandler(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
+        }
     }
 })
 
